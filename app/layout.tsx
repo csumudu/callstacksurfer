7072e8b67a6c2cd65efd0e657f2c46e9c80337df
Sumudu Jayasinghe <sumudu.jayasinghe@csgi.com>
2025-01-04 07:28:03 +0530

    enable analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./css/style.css";
-
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
@@ -25,6 +25,7 @@ export default function RootLayout({
       >
         <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
           {children}
+          <Analytics />
         </div>
       </body>
     </html>
--- components/article-list-item.tsx
+++ components/article-list-item.tsx
@@ -3,7 +3,12 @@ import Link from "next/link";
 export default function ArticleListItem({ article }: any) {
   return (
     <Link href={article.link}>
-      <div className="grid  grid-cols-[10rem_minmax(20rem,_1fr)_5rem] rounded-lg gap-5 p-3  items-center justify-between cursor-pointer border-b bg-gradient-to-r hover:from-gray-50 hover:via-gray-200 hover:to-gray-50">
+      <div
+        className="grid  grid-cols-[10rem_minmax(20rem,_1fr)_5rem] rounded-lg gap-5 p-3  
+      items-center justify-between cursor-pointer border-b 
+      bg-gradient-to-r hover:from-gray-50 hover:via-gray-200 hover:to-gray-50 
+      hover:transition-all hover:duration-300"
+      >
         <div
           className={`bg-center bg-cover size-40 rounded-md drop-shadow-lg`}
           style={{ backgroundImage: `url(${article.thumbnail})` }}
