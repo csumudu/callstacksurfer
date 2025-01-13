af457a570514464922bc0b762cb85318e17f224c
Sumudu Jayasinghe <sumudu.jayasinghe@csgi.com>
2025-01-13 11:46:41 +0530

    fix type

--- app/(articles)/articles/[id]/page.tsx
+++ app/(articles)/articles/[id]/page.tsx
@@ -1,10 +1,14 @@
 import { MarkdownWrapper } from "@/components/markdown-wrapper";
 import { API } from "@/db/sandbox/sandbox";
 
-export default async function Article({ params }: { params: { id: string } }) {
+export default async function Article({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const id = (await params).id;
   const article = await API.getArticleById(id);
-  
+
   return (
     <div className="px-20 prose max-w-full">
       <MarkdownWrapper>{article?.content}</MarkdownWrapper>
