a4fabed12a9834d53ca690c37d7f3befa34e9546
Sumudu Jayasinghe <sumudu.jayasinghe@csgi.com>
2025-01-30 13:17:45 +0530

    add article content

--- components/articles/buble-sort.tsx
+++ components/articles/buble-sort.tsx
@@ -7,8 +7,29 @@ const BubleSort = () => {
       <h3>Bubble Sort in Motion: Watch the Magic of Sorting Unfold!</h3>
 
       <div>
-        {/* <p>todo....</p> */}
+        Bubble Sort is a simple sorting algorithm that repeatedly steps through
+        the list, compares adjacent elements, and swaps them if they are in the
+        wrong order. This process continues until the list is sorted.
       </div>
+      <p>
+        <h4>Steps: </h4>
+        <ul>
+          <li>
+            Start from the first element and compare it with the next one.
+          </li>
+          <li>Swap them if they are in the wrong order.</li>
+          <li>Move to the next pair and repeat.</li>
+          <li>
+            The largest element "bubbles" to the end in each pass. Repeat the
+            process for the remaining elements until no swaps are needed.
+          </li>
+        </ul>
+      </p>
+      <p>
+        You can visualize the Bubble Sort process in the widget below. Try it
+        with your own dataset or generate a random dataset to see how the
+        algorithm works in action!
+      </p>
       <BubbleSortContainer />
     </div>
   );
